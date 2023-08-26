5af6553698f43941657760f90fa2331116f3bc54
Nurmuhammad <nurmuhammad77708@gmail.com>
2023-08-26 23:51:43 +0400

    finished lab_3

--- Lab_3/main.py
+++ Lab_3/main.py
@@ -1,5 +1,6 @@
-
 from pathlib import Path
+import csv
+
 
 def count_files():  # метод для подсчета колво файлов в директории
     k = 0
@@ -11,12 +12,16 @@ def count_files():  # метод для подсчета колво файлов
     print('The number of files in this directory:', k)
 
 
-import csv
-
-
 # Считываем данные из CSV файла в словарь
 def read_csv_to_dict(mdict):
-    with open('data.csv', 'r', encoding='cp1251') as r_file:
+
+    """
+    Ошибка UnicodeDecodeError возникает, когда встречается символ, который невозможно декодировать в кодировку UTF-8.
+    Для решения этой ошибки можно указать правильную кодировку при чтении CSV файла.
+    Попробуйте использовать кодировку "cp1251" (Windows-1251), которая часто используется для файлов CSV, содержащих русские символы.
+    """
+    # Использовать encoding='cp1251', если возникает ошибка UnicodeDecodeError
+    with open('data.csv', 'r') as r_file: #
         file_reader = csv.reader(r_file, delimiter=';')
 
         # Пропускаем заголовок
@@ -38,13 +43,16 @@ def read_csv_to_dict(mdict):
                 'next_step': next_step
 
             }
-    print('Исходные данные:')
+
+    print('Initial data:')
+    for key, value in mdict.items():
+        print(key, '=>', value)
 
 
 # Сохранение данных в файл
 def write_dict_to_csv(mdict):
-    with open('new_data.csv', 'w', newline='') as w_file:
-        file_writer = csv.writer(w_file)
+    with open('data.csv', 'w', newline='') as w_file:
+        file_writer = csv.writer(w_file, delimiter=';')
 
         # Записываем заголовок
         file_writer.writerow(['№', 'телефон', 'взял трубку или нет', 'ФИО клиента', 'дата следующего созвона',
@@ -60,18 +68,107 @@ def write_dict_to_csv(mdict):
 
             file_writer.writerow([call_id, phone, picked_up, fio, next_call_date, next_step])
 
-    print('\nСохранение данных в файл: new_data.csv')
+    print('\nSaving data to a file: new_data.csv')
 
 
-# new_call_info = {
-#     'phone': '555-5555',
-#     'picked_up': False,
-#     'fio': 'Иванов Иван',
-#     'next_call_date': '2022-01-01',
-#     'next_step': 'Позвонить позже'
-# }
-# data_dict[new_call_id] = new_call_info
+# Добавление новых данных в файл
+def add_new_user(mdict):
+    new_call_id = int(input('Enter a new call ID: '))
+    phone = input("Enter client's phone number: ")
+    picked_up = input('Picked up the phone or not: ')
+    fio = input("Client's full name: ")
+    next_call_date = input('Date of the next call: ')
+    next_step = input('Description of the next step for calling: ')
 
+    new_call_info = {
+        'phone': phone,
+        'picked_up': picked_up,
+        'fio': fio,
+        'next_call_date': next_call_date,
+        'next_step': next_step
+    }
+
+    mdict[new_call_id] = new_call_info
+    print('\nUpdated data:')
+    for key, value in mdict.items():
+        print(key, '=>', value)
+
+
+# Сортировка по полю ФИО клиента (строковому)
+def sort_by_string_field(mdict, field):
+    sorted_dict = {}
+
+    sorted_keys = sorted(mdict.keys(), key=lambda x: mdict[x][field])
+
+    for key in sorted_keys:
+        sorted_dict[key] = mdict[key]
+
+    print("\nSorting by the client's full name field (string)")
+    for key, value in sorted_dict.items():
+        print(key, '=>', value)
+
+
+# Сортировка по номеру тел
+def sort_by_numeric_field(mdict, field):
+    sorted_dict = {}
+
+    sorted_keys = sorted(mdict.keys(), key=lambda x: int(mdict[x][field]))
+
+    for key in sorted_keys:
+        sorted_dict[key] = mdict[key]
+
+    print('\nSorting by phone number:')
+    for key, value in sorted_dict.items():
+        print(key, '=>', value)
+
+
+# Фильтрация по полю "взял трубку или нет"
+def filter_by_criteria(data_dict, field, value):
+    filtered_dict = {}
+
+    for key, call_info in data_dict.items():
+        if field in call_info and call_info[field] == value:
+            filtered_dict[key] = call_info
+
+    print('\nFiltering by the field "picked up the phone or not":')
+    for key, value in filtered_dict.items():
+        print(key, '=>', value)
+
+
+def check_n():  # метод для обрабоки ввода
+    i = 0
+    while i < 1:
+        try:
+            print("\nEnter from 0 to 5 to select an option:\n1) Sorting by the client's full name field (string)\n"
+                  "2) Sorting by phone number\n3) Filtering by the field 'picked up the phone or not'\n"
+                  "4) Add a new user\n5) Save to a new .csv file\n0) Exit\n")
+            n: int = int(
+                input('Enter: '))
+            i = 1
+        except ValueError:  # Обработка ошибки
+            print("Enter only a number!")
+    return n
+
+
+def choose_mode_filter():
+
+    while True:
+        num_x = check_n()
+        if num_x == 1:
+            sort_by_string_field(mydict, 'fio')
+        elif num_x == 2:
+            sort_by_numeric_field(mydict, 'phone')
+        elif num_x == 3:
+            filter_by_criteria(mydict, 'picked_up', 'Да')
+        elif num_x == 4:
+            add_new_user(mydict)
+        elif num_x == 5:
+            write_dict_to_csv(mydict)
+        elif num_x == 0:
+            break
+        else:
+            print("Enter only '0' to '5'!")
+            choose_mode_filter()
 
 
 def check_x():  # метод для обрабоки ввода
@@ -92,22 +189,12 @@ def choose_mode():
         count_files()
     elif num_x == 2:
         read_csv_to_dict(mydict)
-        print(mydict)
+        choose_mode_filter()
     else:
-        print("Enter only '0' or '1'!")
+        print("Enter only '1' or '2'!")
         choose_mode()
-
-
 
 
 if __name__ == "__main__":
     mydict = {}
     choose_mode()
-
-    # write_dict_to_csv(mydict)
-
-
-# import os.path
-# path = "/mnt/c/users/end/pictures/wallpaper"
-# num_files = sum(os.path.isfile(os.path.join(path, f)) for f in os.listdir(path))
-# print(num_files)
